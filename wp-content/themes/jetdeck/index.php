c16af65ccd2a4552d5a4afe5844cfe2ac6a68a65
Nate <nate.scott.fisher@gmail.com>
2012-06-10 11:56:30 -0700

    updating static index content

--- wp-content/themes/jetdeck/index.php
+++ wp-content/themes/jetdeck/index.php
@@ -22,12 +22,12 @@ get_header(); ?>
 					<section id="top">
 						<ul id="slider" class="responsiveSlider">
 							<li>
-								<img src="images/img/slide_1.jpg" alt="" class="slide-image" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/img/slide_1.jpg" alt="" class="slide-image" />
 								<div class="slideCaption">
 									<div class="slideCaptionInner">
 										<a style="float:right; margin:6px 0 0;" href="#" class="button_medium"><span>Click Here</span></a>
-										<h2 style="margin:0;">Too big to stay unnoticed</h2>
-										<p style="padding:0;">Cum sociis natoque penatibus et magnis dis parturient montesnascetur ridiculus mus. Pellentesque tristique!</p>
+										<h2 style="margin:0;">There's a Better Way To Send Specs</h2>
+										<p style="padding:0;">Save your customer's inbox and deliver a more professional aircraft presentation</p>
 									</div>
 								</div>
 							</li>
@@ -35,18 +35,18 @@ get_header(); ?>
 								<img src="images/img/slide_2.jpg" alt="" class="slide-image" />
 								<div class="slideCaption">
 									<div class="slideCaptionInner">
-										<h2 style="margin:0;">For your Business</h2>
-										<p style="padding:0;">Cum sociis natoque penatibus et magnis dis parturient montesroin vel nibh</p>
+										<h2 style="margin:0;">Track Who is Watching Your Aircraft</h2>
+										<p style="padding:0;">See real-time activity stats to anticipate a buyer's decision process</p>
 									</div>
 								</div>
 							</li>
 							<li>
-								<img src="images/img/slide_3.jpg" alt="" class="slide-image" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/img/slide_3.jpg" alt="" class="slide-image" />
 								<div class="slideCaption">
 									<div class="slideCaptionInner">
 										<a style="float:right; margin:6px 0 0;" href="#" class="button_medium"><span>Click Here</span></a>
-										<h2 style="margin:0;">Too big to stay unnoticed</h2>
-										<p style="padding:0;">Cum sociis natoque penatibus et magnis dis parturient montesnascetur ridiculus mus. Pellentesque tristique!</p>
+										<h2 style="margin:0;">Never Miss a Deal. Heads Up Info on The Go</h2>
+										<p style="padding:0;">At the office, at the jet or on the move, you'll never overlook a potential sale again.</p>
 									</div>
 								</div>
 							</li>
@@ -60,11 +60,13 @@ get_header(); ?>
 						<div class="entry">
 							<p>&nbsp;</p>
 							<h1 class="aligncenter" style="font-size:30px;">
-							Hello, this is <span class="color_3">JetDeck</span>, a tool specifically for aircraft brokers<br /> to act faster, stay organized and make more deals.</h1>
+							Hello, this is <span class="color_3">JetDeck</span>,
+							an Aircraft Sales Professional's weapon<br /> to act faster,
+							stay organized and close more deals.</h1>
 							<p>&nbsp;</p>
 							<div class="cl"></div>
 							<section class="post_type_shortcode">
-								<h4 class="cms_title">Latest works <span class="widget_navi"><a href="#" class="cmsms_content_prev_slide"></a><a href="#" class="cmsms_content_next_slide"></a></span></h4>
+								<h4 class="cms_title">How It Works <span class="widget_navi"><a href="#" class="cmsms_content_prev_slide"></a><a href="#" class="cmsms_content_next_slide"></a></span></h4>
 								<script type="text/javascript">
 									jQuery(document).ready(function () {
 										jQuery('.post_type_list').cmsmsResponsiveContentSlider( {
@@ -92,16 +94,12 @@ get_header(); ?>
 													</a>
 												</figure>
 												<header class="entry-header">
-													<h4 class="entry-title"><a href="album_four.html">Fully responsive</a></h4>
+													<h4 class="entry-title"><a href="album_four.html">Engage Customers</a></h4>
 												</header>
 												<div class="entry-content">
-													<p>Cum sociis natoque penatibus et magnis dis parturient montes</p>
+													<p>JetDeck focuses on allowing brokers to send more specs, and do it quickly</p>
 												</div>
-												<footer class="entry-meta">
-													<ul class="cms_category">
-														<li><small><a href="#">Branding</a></small></li>
-													</ul>
-												</footer>
+
 											</article>
 										</div>
 										<div class="one_fourth">
@@ -112,16 +110,12 @@ get_header(); ?>
 													</a>
 												</figure>
 												<header class="entry-header">
-													<h4 class="entry-title"><a href="album_four.html">Easy to customize</a></h4>
+													<h4 class="entry-title"><a href="album_four.html">Look Professional</a></h4>
 												</header>
 												<div class="entry-content">
-													<p>Cum sociis natoque penatibus et magnis dis parturient montes</p>
+													<p>Buyers always see up-to-date numbers and impressive presentation</p>
 												</div>
-												<footer class="entry-meta">
-													<ul class="cms_category">
-														<li><small><a href="#">Websites</a></small></li>
-													</ul>
-												</footer>
+
 											</article>
 										</div>
 										<div class="one_fourth">
@@ -132,17 +126,12 @@ get_header(); ?>
 													</a>
 												</figure>
 												<header class="entry-header">
-													<h4 class="entry-title"><a href="album_four.html">Clean & corporative</a></h4>
+													<h4 class="entry-title"><a href="album_four.html">Monitor Activity</a></h4>
 												</header>
 												<div class="entry-content">
 													<p>Cum sociis natoque penatibus et magnis dis parturient montes</p>
 												</div>
-												<footer class="entry-meta">
-													<ul class="cms_category">
-														<li><small><a href="#">Photography</a>,</small></li>
-														<li><small><a href="#">Artwork</a></small></li>
-													</ul>
-												</footer>
+
 											</article>
 										</div>
 										<div class="one_fourth">
@@ -153,16 +142,12 @@ get_header(); ?>
 													</a>
 												</figure>
 												<header class="entry-header">
-													<h4 class="entry-title"><a href="album_four.html">SEO optimised</a></h4>
+													<h4 class="entry-title"><a href="album_four.html">Close Deals</a></h4>
 												</header>
 												<div class="entry-content">
 													<p>Cum sociis natoque penatibus et magnis dis parturient montes</p>
 												</div>
-												<footer class="entry-meta">
-													<ul class="cms_category">
-														<li><small><a href="#">Advertising</a></small></li>
-													</ul>
-												</footer>
+
 											</article>
 										</div>
 									</li>
