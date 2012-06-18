bd345c6e8aa9531fc5c6cc48371bd2faa0d0dbbe
Nate <nate.scott.fisher@gmail.com>
2012-06-17 21:04:09 -0700

    enabling GA

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 <script src="/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <script type="text/javascript">
-/*
+
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-32538435-1']);
   _gaq.push(['_trackPageview']);
@@ -45,7 +45,7 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-*/
+
 </script>
 </head>
 	<body>
