11cb0c564348a34d8b2ae1d750a89f75ab55c9c2
Nate <nate.scott.fisher@gmail.com>
2012-09-03 22:26:29 -0700

    adding timeout to demo ajax post

--- email.php
+++ email.php
@@ -1,7 +1,6 @@
 <?php 
 
-  if ($_SERVER['REMOTE_ADDR'] == $_SERVER['SERVER_ADDR']
-    && $_REQUEST['key'] == '892y4uvn298n24gg2') {
+  if ($_REQUEST['key'] == '892y4uvn298n24gg2') {
   
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
