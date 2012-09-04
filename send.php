00f29ed38105bfec7062b9d267c889fe9ede6821
Nate <nate.scott.fisher@gmail.com>
2012-09-04 01:50:36 -0700

    undo last commit

--- send.php
+++ send.php
@@ -8,7 +8,7 @@
   $email = trim($_REQUEST['email']);
   
   // create url
-  $url = $_SERVER['SERVER_NAME'] . "/specs/4425542.php?name=$name";
+  $url = "http://www.jetdeck.co/specs/4425542.php?name=$name";
   
   // create message 
   if ($name && $url && validEmail($email)) {
