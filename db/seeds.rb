c30461bd36b4c0f8176ec4ee668f247785643a42
thenatefisher <nate.scott.fisher@gmail.com>
2013-01-22 07:25:18 -0700

    updating seed logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'csv'
 CSV.read("#{Rails.root}/db/brokers.csv", {:headers=>true, header_converters: :symbol}).each do |row|
-    user = User.find_or_create_by_email(row[:email])
-    user.update_attributes(row.to_hash.slice(:first, :fullname, :email, :company, :phone, :source))
-    user.update_mp()
+    user = User.find_by_email(row[:email])
+    if user.blank?
+        user = User.create(row.to_hash.slice(:email, :first, :fullname, :email, :company, :phone, :source))
+        user.update_mp()
+    end
 end
