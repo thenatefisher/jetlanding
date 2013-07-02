ded427a455b712aa9b8665343a0644701612035b
thenatefisher <nate.scott.fisher@gmail.com>
2013-07-02 00:45:05 -0700

    updating tos and priv

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,48 +3,17 @@ class IndexController < ApplicationController
   before_filter :sanitize_params
 
   def index
-
-  end
-
-  def support
-    # displays features page
   end
 
   def tos
     # displays tos page
+    render layout: "tos", action: "index"
   end
 
   def priv
     # displays priv pol page
+    render layout: "priv", action: "index"
   end
 
-
-end
-
-class MixPanel
-
-  # event: The overall event/category you would like to log this data under
-  # properties: A hash of key-value pairs that describe the event. Must include
-  # the Mixpanel API token as 'token' and a unqiue id as 'distinct_id'
-  #
-  # See http://mixpanel.com/api/ for further detail.
-  def self.track(event, properties={})
-    return false if !properties.has_key?("token")
-
-    params = {"event" => event, "properties" => properties}
-    data = ActiveSupport::Base64.encode64s(JSON.generate(params))
-    request = "http://api.mixpanel.com/track/?data=#{data}"
-
-    `curl -s '#{request}' &`
-  end
-
-  def self.people(properties={})
-    return false if !properties.has_key?("$token")
-
-    data = ActiveSupport::Base64.encode64s(JSON.generate(properties))
-    request = "http://api.mixpanel.com/engage/?data=#{data}"
-
-    `curl -s '#{request}' &`
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Jetlanding::Application.routes.draw do
 
   match "/priv" => "index#priv"
   match "/tos" => "index#tos"
-  match "/support" => "index#support"
   resources :charges
   root :to => "index#index"
 
