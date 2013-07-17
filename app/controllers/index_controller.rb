48ed5e4c28d42ff82c89f4bb743b555589aa597c
thenatefisher <nate.scott.fisher@gmail.com>
2013-07-16 22:00:37 -0700

    added press kit

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -15,5 +15,20 @@ class IndexController < ApplicationController
     render layout: "priv", action: "index"
   end
 
+  def press_kit
+
+    mixpanel = Mixpanel::Tracker.new(
+      Jetlanding::Application.config.mixpanel_token, 
+      { :async => true, :env => request.env }
+    )
+
+    mixpanel.track_event "Downloaded Press Kit"
+
+    send_file File::open(Rails.root.join('private', 'press_kit.zip')), 
+      :type => 'application/zip',
+      :disposition => 'inline',
+      :filename => "press_kit.zip"
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Jetlanding::Application.routes.draw do
 
   match "/priv" => "index#priv"
   match "/tos" => "index#tos"
-  resources :charges
+  match "/press_kit" => "index#press_kit"
+  match "/press_kit\.zip" => "index#press_kit"
   root :to => "index#index"
 
 end
