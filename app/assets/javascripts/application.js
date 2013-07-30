777de8b28bddfddbfb7890689cc93164972638b0
thenatefisher <nate.scott.fisher@gmail.com>
2013-07-29 22:59:11 -0700

    adding flexslider and feature item about tracking leads

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,28 @@
 //= require jquery
 //= require jquery_ujs
 //= require lib/twitter
+//= require flexslider
 //= require lib/bootstrap-modal
+
+// smooth scrolling elegantly provided by Hanoo 
+// (http://stackoverflow.com/questions/4198041/jquery-smooth-scroll-to-an-anchor)
+$(".scroll").click(function(event){
+   event.preventDefault();
+   //calculate destination place
+   var dest=0;
+   var offset=50;
+   if($(this.hash).offset().top > $(document).height()-$(window).height()-offset){
+        dest=$(document).height()-$(window).height()-offset;
+   }else{
+        dest=$(this.hash).offset().top-offset;
+   }
+   //go to destination
+   $('html,body').animate({scrollTop:dest}, 1000,'swing');
+});
+
+// flexslider   
+$(function () { 
+	if ($(".flexslider").length) {
+	    $('.flexslider').flexslider();
+	}
+});
